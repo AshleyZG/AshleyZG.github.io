015556cbf0e381a5f99d69427f199edfd2e61b44
AshleyZG <gsarazh@gmail.com>
2023-07-03 13:51:30 -0400

    author style

--- javascripts/pubs.js
+++ javascripts/pubs.js
@@ -1,6 +1,6 @@
 const LongPubsSection = document.getElementById("pubs-long");
 const ShortPubsSection = document.getElementById("pubs-short");
-
+const MYNAME = "Ashley Ge Zhang";
 // 
 /**Pub data structure
  * {
@@ -24,7 +24,7 @@ const ShortPubsSection = document.getElementById("pubs-short");
 const longPubs = [
     {
         title: "RunEx: Augmenting Regular-Expression Code Search with Runtime Values",
-        authors: ["Ashley Ge Zhang", "Yan Chen", "Steve Oney"],
+        authors: [MYNAME, "Yan Chen", "Steve Oney"],
         venue: "VL/HCC 2023",
         abstractHTML: "Introduced RunEx, a novel code search tool for programming instructors, where they can effortlessly generate queries with minimal prior knowledge of code search and rapidly search through a large code corpus. RunEx consists of a syntax that augments regular expression with runtime values, and a user interface that enables instructors to construct runtime and syntax-based queries.",
     // optional
@@ -35,7 +35,7 @@ const longPubs = [
     }, 
     {
         title: "VizProg: Identifying Misunderstandings by Visualizing Students' Coding Progress",
-        authors: ["Ashley Ge Zhang", "Yan Chen", "Steve Oney"],
+        authors: [MYNAME, "Yan Chen", "Steve Oney"],
         venue: "CHI 2023",
         abstractHTML: `Proposed a VizProg, a system that visualizing students' progress in in-class programming exercises by encoding their status into a 2-D map view. A comparison experiment showed that VizProg helped instructors identify more misunderstandings accurately than the baseline system (a real-time version of <a href="https://roc-hci.com/wp-content/uploads/overcode-visualizing-programming-solutions_TOCHI-2015.pdf">OverCode</a>). 
         </p>`,
@@ -46,7 +46,7 @@ const longPubs = [
     },
     {
         title: "Colaroid: A Literate Programming Approach for Authoring Explorable Multi-Stage Tutorials",
-        authors: ["April Wang", "Andrew Head", "Ashley Ge Zhang", "Steve Oney", "Christopher Brooks"],
+        authors: ["April Wang", "Andrew Head", MYNAME, "Steve Oney", "Christopher Brooks"],
         venue: "CHI 2023",
         abstractHTML: "Colaroid allows users to create web programming tutorials in augmented computational notebooks, where each cell represents a snapshot of the project. In each cell, users can check the complete source code context, highlighted code differences, and the executed outputs. Users can also load and tinker with any stage of the project in a linked IDE.",
         // optional
@@ -57,7 +57,7 @@ const longPubs = [
     },
     {
         title: "CORAL: COde RepresentAtion Learning with Weakly-Supervised Transformers for Analyzing Data Analysis",
-        authors: ["Ashley Ge Zhang*", "Mike Merrill*", "Yang Liu", "Jeffrey Heer", "Tim Althoff"],
+        authors: [MYNAME+"*", "Mike Merrill*", "Yang Liu", "Jeffrey Heer", "Tim Althoff"],
         venue: "EPJ Data Science 2022",
         abstractHTML: "Proposed a novel weakly supervised transformer-based architecture for computing joint representations of code to uncover common data analysis patterns from a set of 118,000 Jupyter Notebooks.",
         // optional
@@ -68,7 +68,7 @@ const longPubs = [
     },
     {
         title: "Mining Collective Data Science Knowledge from Code on the Web to Suggest Alternative Data Analysis Approaches",
-        authors: ["Mike Merril", "Ashley Ge Zhang", "Tim Althoff"],
+        authors: ["Mike Merril", MYNAME, "Tim Althoff"],
         venue: "KDD 2021",
         abstractHTML: "Proposed a graph neural network model to support novice data analysts in exploring alternative approaches by identifying decision points and suggesting alternatives in computational notebooks for data science.",
         // optional
@@ -82,7 +82,7 @@ const longPubs = [
 const shortPubs = [
     {
         title: "How Pairing by Code Similarity Influences Discussions in Peer Learning",
-        authors: ["Shiyu Xu", "Ashley Ge Zhang", "Steve Oney"],
+        authors: ["Shiyu Xu", MYNAME, "Steve Oney"],
         venue: "CHI 2023 Late-Breaking Work",
         abstractHTML: "Peer learning has been widely used in programming courses as a means of promoting active learning and enhancing students' programming skills. We investigated in what ways and to what extent groupding students with similar or different solutions affect the discussions that take place within groups.",
         // optional
@@ -114,9 +114,20 @@ function createImgDiv(imgPath){
 
 function createAuthorsSpan(authors){
     const authorsSpan = document.createElement('span');
-    authorsSpan.classList.add('authors');
-    const authorsNode = document.createTextNode(authors);
-    authorsSpan.appendChild(authorsNode);
+    authorsSpan.classList.add('author-list');
+    for (const [index, author] of authors.entries()) {
+        var authorNode;
+        if (author === MYNAME || author === MYNAME+"*"){
+            authorNode = document.createElement("strong");
+        }else{
+            authorNode = document.createElement("span");
+        }
+        authorNode.classList.add('author');
+        if (index === 0){authorNode.classList.add('no-before');}
+        authorNode.innerText = author;   
+        authorsSpan.appendChild(authorNode);             
+
+    }
     return authorsSpan;
 }
 
