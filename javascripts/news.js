59dbe5a41ed74b40d949e6df2e56f5ae16cb4816
AshleyZG <gsarazh@gmail.com>
2023-09-28 16:23:29 -0400

    add news

--- javascripts/news.js
+++ javascripts/news.js
@@ -1,6 +1,7 @@
 const NewsDiv = document.getElementById("news");
 
 const news = [
+    {date: "Oct 2023", content: "I will present RunEx at VL/HCC. See you all in Washington DC. "},
     {date: "June 2023", content: "One paper is conditionally accepted to VL/HCC 2023!"},
     {date: "April 2023", content: "VizProg and Colaroid won Best Paper Honorable Mention Awards at CHI 2023!"},
     {date: "Jan 2023", content: "Two papers are conditionally accepted to CHI 2023!"}
